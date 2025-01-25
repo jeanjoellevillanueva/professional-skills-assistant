c499c5d97cf6032a31c484dd0e9ef818e15df999
jeanjoellevillanueva <jeanjoellevillanueva@gmail.com>
2025-01-25 20:48:06 +0800

    Working AI utils

--- backend/ai_utils.py
+++ backend/ai_utils.py
@@ -3,11 +3,15 @@ import os
 import openai
 from dotenv import load_dotenv
 from fastapi import HTTPException
+from openai import OpenAI
 
 
 load_dotenv()
+client = OpenAI()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+openai.api_key = OPENAI_API_KEY
 LLM_MODEL = os.getenv('LLM_MODEL')
+
 
 
 def answer_question(question: str) -> str:
@@ -16,14 +20,19 @@ def answer_question(question: str) -> str:
     """
 
     try:
-        response = openai.Completion.create(
-            engine=LLM_MODEL,
-            prompt=question,
-            max_token=250,
+        response = client.chat.completions.create(
+            model=LLM_MODEL,
+            messages=[
+                {
+                    'role': 'developer',
+                    'content': 'You are a helpful assistant that will answer the question about the user\'s information.'},
+                {
+                    'role': 'user',
+                    'content': question
+                }
+            ],
             temperature=0.6,
         )
-        import pdb; pdb.set_trace()
-        return response.choice[0].text.strip()
+        return response.choices[0].message.content
     except Exception as e:
-        import pdb; pdb.set_trace()
         raise HTTPException(status_code=500, detail=f'Error generating response: {e}')
